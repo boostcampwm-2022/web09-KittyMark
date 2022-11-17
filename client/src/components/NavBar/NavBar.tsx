97c3748bb0c0b07daff6dd6c898f7a7cce409033
yooji0415 <gch05406@gmail.com>
2022-11-17 17:35:48 +0900

    feat: 데모용 home 페이지 추가 및 하단 컴포넌트 생성

---
+++ client/src/components/NavBar/NavBar.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+// img
+import homeIcon from '../../static/homeIcon.svg';
+import mapIcon from '../../static/mapIcon.svg';
+import messageIcon from '../../static/messageIcon.svg';
+import userIcon from '../../static/userIcon.svg';
+// style
+import { NavBarContainer, NavBarIcon } from './NavBarStyles';
+
+const NavBar = () => {
+	return (
+		<NavBarContainer>
+			<NavBarIcon type="button">
+				<img src={homeIcon} alt="Home" />
+			</NavBarIcon>
+			<NavBarIcon type="button">
+				<img src={mapIcon} alt="Home" />
+			</NavBarIcon>
+			<NavBarIcon type="button">
+				<img src={messageIcon} alt="Home" />
+			</NavBarIcon>
+			<NavBarIcon type="button">
+				<img src={userIcon} alt="Home" />
+			</NavBarIcon>
+		</NavBarContainer>
+	);
+};
+
+export default NavBar;
