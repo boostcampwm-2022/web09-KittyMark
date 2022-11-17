661feba7abcf4046caaf85c008069ddcb74af887
dla0510 <dla0510@naver.com>
2022-11-18 02:48:40 +0900

    feat: user 모듈 생성

---
+++ server/src/user/user.controller.ts
@@ -0,0 +1,13 @@
+import { Controller } from '@nestjs/common';
+import { UserService } from './user.service';
+
+@Controller('user')
+export class UserController {
+	userService: UserService;
+
+	constructor(userService: UserService) {
+		this.userService = userService;
+	}
+
+	// TODO: 회원가입
+}
---
+++ server/src/user/user.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { DatabaseModule } from 'src/database/database.module';
+import { userProviders } from './user.providers';
+import { UserService } from './user.service';
+
+@Module({
+	imports: [DatabaseModule],
+	providers: [...userProviders, UserService],
+	exports: [...userProviders, UserService],
+})
+export class UserModule {}
