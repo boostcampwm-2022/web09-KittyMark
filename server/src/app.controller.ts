2ea61b8b037833a8fa3aa8090913b0e1d59d85d6
wooseong <dominance5995@gmail.com>
2022-11-20 15:50:36 +0900

    style: server 폴더 prettier 포맷 적용

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,5 +1,4 @@
-import { CACHE_MANAGER, Controller, Get, Inject } from '@nestjs/common';
-import { Cache } from 'cache-manager';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
