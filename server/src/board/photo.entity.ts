50f870d4ac3a9d91a976cd0c800d9747f91a06c6
wooseong <dominance5995@gmail.com>
2022-11-23 07:53:31 +0900

    feat: board 엔티티 연관관계 설정

---
+++ server/src/board/photo.entity.ts
@@ -0,0 +1,14 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Board } from './board.entity';
+
+@Entity()
+export class Photo {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  url: string;
+
+  @ManyToOne(() => Board, (board) => board.photos)
+  board: Board;
+}
