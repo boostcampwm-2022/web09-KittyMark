2d577455ae71ec90caf3badbbf3eae5c8a796aef
wooseong <dominance5995@gmail.com>
2022-11-23 01:31:41 +0900

    feat: snakecase 자동변환 모듈 추가

--- server/src/configs/typeorm.config.ts
+++ server/src/configs/typeorm.config.ts
@@ -1,6 +1,8 @@
 import * as dotenv from 'dotenv';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { User } from 'src/user/user.entity';
+import { Board } from '../board/board.entity';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 
 dotenv.config();
 
@@ -11,6 +13,7 @@ export const typeORMConfig: TypeOrmModuleOptions = {
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-  entities: [User],
+  entities: [User, Board],
   synchronize: true,
+  namingStrategy: new SnakeNamingStrategy(),
 };
