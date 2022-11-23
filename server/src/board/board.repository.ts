7f3d0fafccf85c3c6049077b14282169c8874493
wooseong <dominance5995@gmail.com>
2022-11-24 00:18:49 +0900

    chore: board Repository 기능 예정

--- server/src/board/board.repository.ts
+++ server/src/board/board.repository.ts
@@ -1,15 +1,25 @@
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Board } from './board.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class BoardRepository {
   constructor(
     @InjectRepository(Board) private boardRepository: Repository<Board>,
   ) {}
-
-  // async save(board) {
-  //   return this.boardRepository.create(board);
-  // }
+  //
+  //   async save(board: Board) {
+  //     await this.boardRepository.save(board);
+  //   }
+  //
+  //   async create(board: Board) {
+  //     await this.boardRepository.create(board);
+  //   }
+  //
+  //   async update(id, updatedData) {
+  //     await this.boardRepository.update(id,updatedData);
+  //   }
+  //
+  //   async getLastBoardList(count, number) {}
 }
