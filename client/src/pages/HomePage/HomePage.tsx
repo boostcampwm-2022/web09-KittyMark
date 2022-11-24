562415c329cdb4d6a1188041b7c2fc8959709436
hjy0951 <hjy0951@gmail.com>
2022-11-24 16:26:35 +0900

    feat: Home 페이지 게시글 데이터 요청 API 추가

--- client/src/pages/HomePage/HomePage.tsx
+++ client/src/pages/HomePage/HomePage.tsx
@@ -1,21 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 // img
 import testImage from '../../static/testImage.png';
 import catFootprint from '../../static/catFootprint.svg';
 // component
 import NavBar from '../../components/NavBar/NavBar';
 import NormalTopBar from '../../components/NormalTopBar/NormalTopBar';
+import BoardItem from '../../components/BoardItem/BoardItem';
 // style
 import { BoardContainer, BoardEnd } from './HomePageStyles';
-import BoardItem from '../../components/BoardItem/BoardItem';
+// type
 import { Board } from '../../types/responseData';
+// api
+import { getBoardData } from '../../apis/api/BoardApi';
 
 /* Test Data */
 const today = new Date(Date.now());
 const timeStamp = `${today.getFullYear()}.${
   today.getMonth() + 1
 }.${today.getDate()}`;
-const boards: Board[] = [
+const dummyBoards: Board[] = [
   {
     id: '1',
     content: '게시글 테스트1',
@@ -70,11 +73,35 @@ const boards: Board[] = [
 ];
 
 const HomePage = () => {
+  const [boards, setBoards] = useState<Board[]>([]);
+  const requestCount = 10000;
+  const getData = async () => {
+    const { statusCode, message, data } = await getBoardData(
+      requestCount,
+      '-1',
+    );
+    if (statusCode !== 200) throw new Error(message);
+    if (data.boards === undefined) return;
+    setBoards(boards.concat(data.boards));
+  };
+
+  useEffect(() => {
+    try {
+      getData();
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.log(error);
+    }
+  }, []);
+
   return (
     <>
       <NormalTopBar />
       <BoardContainer>
         {boards.map((board) => {
+          return BoardItem(board);
+        })}
+        {dummyBoards.map((board) => {
           return BoardItem(board);
         })}
         <BoardEnd>
