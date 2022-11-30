fe6960ca55da5625041fb26de0c870822a04ce56
hjy0951 <hjy0951@gmail.com>
2022-11-30 17:03:01 +0900

    feat: 위/경도 추출 util 함수 추가

---
+++ client/src/utils/map.ts
@@ -0,0 +1,41 @@
+import { Board } from '../types/responseData';
+
+interface Coordinate {
+  latitude: number;
+  longitude: number;
+}
+
+const createPointFromSize = (width: number, height: number) => {
+  return new naver.maps.Point(width, height);
+};
+
+const convertPointToCoord = (map: naver.maps.Map, point: naver.maps.Point) => {
+  const mapProjection = map.getProjection();
+  const coord = mapProjection.fromOffsetToCoord(point);
+  return coord;
+};
+
+const getCoord = (map: naver.maps.Map, width: number, height: number) => {
+  const point = createPointFromSize(width, height);
+  const coord = convertPointToCoord(map, point);
+  const { y: latitude, x: longitude } = coord;
+  return { latitude, longitude };
+};
+
+const getQueryMapRange = (map: naver.maps.Map) => {
+  const { width, height } = map.getSize();
+  const leftTop = getCoord(map, 0, 0);
+  const rightDown = getCoord(map, width, height);
+  return { leftTop, rightDown };
+};
+
+const extractCoord = (boards: Board[]) => {
+  const coordList: Coordinate[] = [];
+  boards.forEach((board) => {
+    const coord = { latitude: board.latitude, longitude: board.longitude };
+    coordList.push(coord);
+  });
+  return coordList;
+};
+
+export { getQueryMapRange, extractCoord };
