e11fd81381797fa227a90f9e8a17f25c54dae57c
yooji0415 <gch05406@gmail.com>
2022-12-01 14:35:56 +0900

    feat: 사용자 좌표를 통해서 지역 이름을 받아오는 api 생성

---
+++ client/src/apis/services/reverseGeocodingService.ts
@@ -0,0 +1,16 @@
+import getReverseGeoData from '../api/reverseGeocoding';
+
+const getlocationData = async (latitude: number, longitude: number) => {
+  const data = await getReverseGeoData(latitude, longitude);
+  if (data.status.name !== 'ok')
+    throw Error('지도 경로 처리에 실패하였습니다.');
+
+  const { region } = data.results[0];
+  const areas = Object.values(region).map((value, idx) => {
+    if (idx !== 0 && value.name !== '') return value.name;
+    return '';
+  });
+  return areas.join(' ');
+};
+
+export default getlocationData;
