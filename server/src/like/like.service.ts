898a68fbf6258791e51037e001539508b2514316
wooseong <dominance5995@gmail.com>
2022-12-01 17:45:17 +0900

    feat: 좋아요 API 작성

---
+++ server/src/like/like.service.ts
@@ -0,0 +1,45 @@
+import { ConflictException, Injectable } from '@nestjs/common';
+import { LikeBoardDto } from 'board/dto/like-board.dto';
+import { BoardRepository } from 'board/board.repository';
+import { UserRepository } from '../user/user.repository';
+import { LikeRepository } from './like.repository';
+import { Like } from './like.entity';
+import { plainToInstance } from 'class-transformer';
+
+@Injectable()
+export class LikeService {
+  constructor(
+    private readonly boardRepository: BoardRepository,
+    private readonly userRepository: UserRepository,
+    private readonly likeRepository: LikeRepository,
+  ) {}
+
+  async boardLike(likeBoardDto: LikeBoardDto) {
+    const { userId, boardId } = likeBoardDto;
+    const isDistinct = await this.likeRepository.findDistinctLike(
+      boardId,
+      userId,
+    );
+
+    if (isDistinct) {
+      throw new ConflictException('이미 좋아요를 눌렀습니다.');
+      return;
+    }
+
+    const board = await this.boardRepository.findOneById(boardId);
+    const user = await this.userRepository.findById(userId);
+    const like = plainToInstance(Like, { board, user });
+    await this.likeRepository.save(like);
+    return;
+  }
+
+  async boardUnLike(likeBoardDto: LikeBoardDto) {
+    const { userId, boardId } = likeBoardDto;
+    await this.likeRepository.deleteByUserBoardId(userId, boardId);
+    return;
+  }
+
+  async getBoardLikeList(boardId: number) {
+    return await this.boardRepository.findLikeListByBoardId(boardId);
+  }
+}
