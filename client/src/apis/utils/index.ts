326788cb0d4cec22beed00ad85ae12eea5f22b21
yooji0415 <gch05406@gmail.com>
2022-12-01 15:10:26 +0900

    refactor: 주소 확인 이후 위도, 경도, 주소 정보를 포함한 post 요청

--- client/src/apis/utils/index.ts
+++ client/src/apis/utils/index.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { SERVER, TESTSERVER } from '../config';
 
 // SERVER 연결시 SERVER 로 변경 / 테스트시 TESTSERVER 로 변경
-const BASE_URL = SERVER;
+const BASE_URL = TESTSERVER;
 
 const axiosApi = (url: string, headers?: Record<string, unknown>) => {
   return axios.create({ baseURL: url, headers });
