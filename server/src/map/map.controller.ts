87aff94e67e9e96d9c7ff4dee08e124ea3ec2a62
wooseong <dominance5995@gmail.com>
2022-12-02 12:33:35 +0900

    feat: 서버에서 네이버지도로 정보요청

--- server/src/map/map.controller.ts
+++ server/src/map/map.controller.ts
@@ -1,13 +1,16 @@
 import {
   Body,
   Controller,
+  Get,
   Post,
+  Req,
   UseInterceptors,
   ValidationPipe,
 } from '@nestjs/common';
 import { GetMapDto } from './dto/get-map.dto';
 import { ResponseInterceptor } from '../interceptor/responseInterceptor';
 import { MapService } from './map.service';
+import { Request } from 'express';
 
 @Controller('map')
 @UseInterceptors(ResponseInterceptor)
@@ -17,5 +20,10 @@ export class MapController {
   getBoardList(@Body(ValidationPipe) getMapDto: GetMapDto) {
     console.log(getMapDto);
     return this.mapService.findWithInRange(getMapDto);
+  }
+
+  @Get('/map-reversegeocode/*')
+  getUserLocation(@Req() req: Request) {
+    return this.mapService.getGeoLocation(req);
   }
 }
--- server/src/map/map.service.ts
+++ server/src/map/map.service.ts
@@ -1,11 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { GetMapDto } from './dto/get-map.dto';
 import { BoardRepository } from 'board/board.repository';
+import { HttpService } from '@nestjs/axios';
+import { firstValueFrom, map } from 'rxjs';
+import { Request } from 'express';
 
 @Injectable()
 export class MapService {
-  constructor(private readonly boardRepository: BoardRepository) {}
+  constructor(
+    private readonly boardRepository: BoardRepository,
+    private readonly httpService: HttpService,
+  ) {}
   async findWithInRange(getMapDto: GetMapDto) {
     return await this.boardRepository.findWithInRange(getMapDto);
+  }
+
+  async getGeoLocation(req: Request) {
+    const option = {
+      method: 'get',
+      url:
+        'https://naveropenapi.apigw.ntruss.com' + req.url.replace('/map', ''),
+      headers: {
+        'X-NCP-APIGW-API-KEY-ID': process.env.NAVER_MAPS_CLIENT_ID,
+        'X-NCP-APIGW-API-KEY': process.env.NAVER_MAPS_CLIENT_SECRET,
+      },
+    };
+    const response = this.httpService.request(option).pipe(map((r) => r.data));
+    return await firstValueFrom(response);
   }
 }
