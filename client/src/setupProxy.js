034cee3327f74976e6bf63737258e06c2a34bb62
wooseong <dominance5995@gmail.com>
2022-12-02 12:32:42 +0900

    chore: 프록시 제거

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,24 +3,12 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = (app) => {
   app.use(
-    createProxyMiddleware('/oauth2.0', {
-      target: 'https://nid.naver.com',
-      changeOrigin: true,
-    }),
-  );
-  app.use(
     createProxyMiddleware('/api', {
       target: 'http://localhost:4000',
       changeOrigin: true,
       pathRewrite: {
         '^/api/': '/', // remove base path
       },
-    }),
-  );
-  app.use(
-    createProxyMiddleware('/map-reversegeocode', {
-      target: `https://naveropenapi.apigw.ntruss.com`,
-      changeOrigin: true,
     }),
   );
 };
