39dc36b6c5335a445cc3ea39e2ce15ac323eff98
yooji0415 <gch05406@gmail.com>
2022-12-05 22:42:40 +0900

    feat: 보드 상세정보 페이지 및 보드 내부 데이터 recoil atom 생성

---
+++ client/src/pages/BoardDetailPage/BoardDetailPageStyles.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const Wrap = styled.div`
+  font-family: 'Jua';
+  font-style: normal;
+  min-height: 100vh;
+  overflow: hidden;
+  width: 100vw;
+  height: 90%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export default { Wrap };
