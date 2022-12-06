4522ddc1e0a0255a0c13049e4b79b7a1da020ec2
yooji0415 <gch05406@gmail.com>
2022-12-06 16:56:49 +0900

    refactor: 페이지 코드 스플릿 및 로딩페이지 적용

---
+++ client/src/pages/index.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import Loadable from 'react-loadable';
+import LoadingContainer from '../components/LoadingContainer/LoadingContainer';
+
+const Loading = () => {
+  return <LoadingContainer />;
+};
+
+export const LoadingPage = Loadable({
+  loader: () => import('./LoadingPage/LoadingPage'),
+  loading: Loading,
+});
+
+export const MapPage = Loadable({
+  loader: () => import('./MapPage/MapPage'),
+  loading: Loading,
+});
+
+export const HomePage = Loadable({
+  loader: () => import('./HomePage/HomePage'),
+  loading: Loading,
+});
+
+export const RegisterPage = Loadable({
+  loader: () => import('./RegisterPage/RegisterPage'),
+  loading: Loading,
+});
+
+export const CommentPage = Loadable({
+  loader: () => import('./CommentPage/CommentPage'),
+  loading: Loading,
+});
+
+export const NewPostPage = Loadable({
+  loader: () => import('./NewPostPage/NewPostPage'),
+  loading: Loading,
+});
+
+export const ModifyPage = Loadable({
+  loader: () => import('./ModifyPage/ModifyPage'),
+  loading: Loading,
+});
+
+export const UserPage = Loadable({
+  loader: () => import('./UserPage/UserPage'),
+  loading: Loading,
+});
+
+export const FollowPage = Loadable({
+  loader: () => import('./FollowPage/FollowPage'),
+  loading: Loading,
+});
+
+export const ModifyUserPage = Loadable({
+  loader: () => import('./ModifyUserPage/ModifyUserPage'),
+  loading: Loading,
+});
+
+export const BoardDetailPage = Loadable({
+  loader: () => import('./BoardDetailPage/BoardDetailPage'),
+  loading: Loading,
+});
